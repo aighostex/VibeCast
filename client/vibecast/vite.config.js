b10051d7a0f18aa1ec4d7a119a5c8aba6715fc42
Ismail-fakai <ismailaliyu8624@gmail.com>
2025-07-01 00:53:36 +0100

    fix

--- client/vibecast/vite.config.js
+++ client/vibecast/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: import.meta.env.VITE_BASE_PATH || "/VibeCast",
+  // base: import.meta.env.VITE_BASE_PATH || "/VibeCast",
   server: {
     port: 3000,
     allowedHosts: true
