5c5efc8bb68c9e0f45e2b9ada1a2c9e04604a34a
Ismail-fakai <ismailaliyu8624@gmail.com>
2025-07-01 12:00:24 +0100

    resolved assets

--- client/vibecast/src/components/Keyfeatures.jsx
+++ client/vibecast/src/components/Keyfeatures.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import analytics from '/src/assets/analytics.svg'
+import engagement from '/src/assets/engagement.svg'
+import audience from '/src/assets/audience.svg'
+import growth from '/src/assets/growth.svg'
 
 
 function Keyfeatures() {
@@ -7,18 +11,18 @@ function Keyfeatures() {
       <h1 className="keyf">Key Features</h1>
       <p className='keyf1'>Powerful analytics to help you understand your audience better</p>
 
-      <div className="grid sm:grid-cols-2 gap-5">
+      <div className="grid sm:grid-cols-2 gap-5 mt-5">
         <div className='feat'>
-            <img src="/src/assets/analytics.svg" alt="" />
+            <img src={analytics} alt="" />
         </div>
         <div className='feat'>
-            <img src="/src/assets/engagement.svg" alt="" />
+            <img src={engagement} alt="" />
         </div>
         <div className='feat'>
-            <img src="/src/assets/audience.svg" alt="" />
+            <img src={audience} alt="" />
         </div>
         <div className='feat'>
-            <img src="/src/assets/growth.svg" alt="" />
+            <img src={growth} alt="" />
         </div>
       </div>
       <p className="keynote">*All this are Dummy data. Sign in to analyze your own audience.</p>
